418b855af1be1c59f367d4354a86172820ac884c
Yakir Sitbon <kingyes1@gmail.com>
2014-04-17 18:29:09 +0000

    Added metabox; savepost; nonce; redirect with counter

--- classes/class-linker-cpt.php
+++ classes/class-linker-cpt.php
@@ -33,14 +33,81 @@ class Linker_CPT {
 				'with_front' => false
 			),
 		);
-
+		
 		register_post_type( 'linker',
 			apply_filters( 'linker_register_post_type_args', $args )
 		);
 	}
+
+	public function register_meta_box() {
+		add_meta_box(
+			'linker-url-information',
+			__( 'URL Information', 'linker' ),
+			array( &$this, 'render_meta_box' ),
+			'linker',
+			'normal',
+			'high'
+		);
+	}
+
+	public function render_meta_box( $post ) {
+		wp_nonce_field( basename( __FILE__ ), '_linker_meta_box_nonce' );
+		
+		$field_id = '_linker_redirect';
+		echo strtr( '<p><strong><label for="{name}">{label}</label></strong></p><p><input type="url" id="{name}" name="{name}" value="{value}" placeholder="{placeholder}" class="large-text" /></p>', array(
+			'{label}' => __( 'Redirect URI:', 'linker' ),
+			'{name}'  => $field_id,
+			'{placeholder}' => __( 'http://example.com/your-link', 'linker' ),
+			'{value}' => esc_attr( get_post_meta( $post->ID, $field_id, true ) ),
+		) );
+
+		$counter = absint( get_post_meta( $post->ID, '_linker_count', true ) );
+		printf( '<p class="description">' . __( 'This URL has been accessed <strong>%d</strong> times.', 'linker' ) . '</p>', $counter );
+	}
+
+	public function save_post( $post_id ) {
+		if ( ! isset( $_POST['_linker_meta_box_nonce'] ) || ! wp_verify_nonce( $_POST['_linker_meta_box_nonce'], basename( __FILE__ ) ) )
+			return;
+
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+			return;
+
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX )
+			return;
+
+		if ( defined( 'DOING_CRON' ) && DOING_CRON )
+			return;
+		
+		if ( isset( $_POST['_linker_redirect'] ) )
+			update_post_meta( $post_id, '_linker_redirect', $_POST['_linker_redirect'] );
+		else
+			delete_post_meta( $post_id, '_linker_redirect' );
+	}
+
+	public function count_and_redirect() {
+		if ( ! is_singular( 'linker' ) )
+			return;
+
+		$counter = absint( get_post_meta( get_the_ID(), '_linker_count', true ) );
+		update_post_meta( get_the_ID(), '_linker_count', ++$counter );
+
+		$redirect_url = esc_url_raw( get_post_meta( get_the_ID(), '_linker_redirect', true ) );
+		
+		if ( ! empty( $redirect_url ) )
+			wp_redirect( $redirect_url, 301 );
+		else
+			wp_redirect( home_url(), 302 );
+		
+		die();
+	}
 	
 	public function __construct() {
+		// TODO: please add updated messages
+		
 		add_action( 'init', array( &$this, 'register_post_type' ) );
+		add_action( 'admin_menu', array( &$this, 'register_meta_box' ) );
+		add_action( 'save_post', array( &$this, 'save_post' ) );
+		add_action( 'template_redirect', array( &$this, 'count_and_redirect' ) );
 	}
 	
 }
